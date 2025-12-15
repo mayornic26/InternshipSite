63f4b857f394ada9aa77af3d347eea57c639a18e
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-12-14 20:08:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const app = express();
 const pool = require("./db"); 
 
 app.use(express.json());
-app.use(express.static("public")); 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.send("Welcome to The Website!");
 });
 
 
 app.get("/users", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM users");
-        res.json(result.rows);
+        res.json(result.rows); 
     } catch (err) {
         console.error("Error fetching users:", err);
         res.status(500).send("Error fetching users");
@@ -23,7 +22,7 @@ app.get("/users", async (req, res) => {
 app.get("/alumni", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM alumni_profiles");
-        res.json(result.rows);
+        res.json(result.rows); 
     } catch (err) {
         console.error("Error fetching alumni:", err);
         res.status(500).send("Error fetching alumni");
